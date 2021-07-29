447fb98649fb9cb23b5882a74ec3d42886b69f7c
admin <admin@YD03-21>
2021-07-29 17:32:24 +0900

    mywebsite

---
+++ mywebsite/src/main/webapp/WEB-INF/views/member/memberList.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div align="center">
+	<div><h1>회원목록 리스트겠냐?</h1></div>
+	<div><h1>여기회원 정보 나오겠냐?</h1></div>	
+</div>
+</body>
+</html>
--- prj/src/main/java/co/micol/prj/member/web/MemberList.java
+++ prj/src/main/java/co/micol/prj/member/web/MemberList.java
@@ -1,16 +1,27 @@
 package co.micol.prj.member.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.micol.prj.common.Command;
+import co.micol.prj.member.service.MemberService;
+import co.micol.prj.member.serviceImpl.MemberServiceImpl;
+import co.micol.prj.member.vo.MemberVO;
 
 public class MemberList implements Command {
 
 	@Override
-	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	   public String execute(HttpServletRequest request, HttpServletResponse response) {
+	      // TODO 회원 전체 목록 보기
+	      MemberService dao = new MemberServiceImpl(); //모델을 호출
+	      List<MemberVO> members = new ArrayList<MemberVO>();
+	      members = dao.memberSelectList();   //데이터를 가져옴
+	      request.setAttribute("list", members);   //페이지에 값을 전달하기 위해
+	      return "member/memberList";      //보여줄 페이지 선택
+	   }
+	
 
 }
