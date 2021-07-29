3ab415b24aa013dc3da2add4839953a3f8f61a3a
admin <admin@YD03-21>
2021-07-29 12:13:47 +0900

    dsadsa

---
+++ prj/src/main/java/co/micol/prj/web/TestController.java
@@ -0,0 +1,52 @@
+package co.micol.prj.web;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.micol.prj.dao.DataSource;
+
+/**
+ * Servlet implementation class TestController
+ */
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DataSource dataSource = DataSource.getInstance();  //인스턴스 가져오기
+		Connection conn = dataSource.getConnection(); //컨넥션 가져오기
+		
+		if(conn != null) {
+			System.out.println("DB 연결 성공!!!!!!");
+		} else {
+			System.out.println("DB 연결 실패??????");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
